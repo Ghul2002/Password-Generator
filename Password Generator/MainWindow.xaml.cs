34b4df51d27c25ab2946c1afb9a44f7e3d2b4c62
Jakub Rosak <ghul2002@o2.pl>
2023-05-08 18:46:08 +0200

    Small tweaks

--- Password Generator/MainWindow.xaml.cs
+++ Password Generator/MainWindow.xaml.cs
@@ -24,95 +24,31 @@ namespace Password_Generator
         public MainWindow()
         {
             InitializeComponent();
+            this.Style = (Style)FindResource(typeof(Window));
         }
 
-        private void B1_Click(object sender, RoutedEventArgs e)
+        private void CopyBT_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(PasswordTB.Text);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            val = Convert.ToInt32(Slider.Value);
+            CharNumberTB.Text = val.ToString();
+        }
+
+        private void GenBT_Click(object sender, RoutedEventArgs e)
         {
             RandomPaswwordGen r1 = new RandomPaswwordGen();
-            if(UpperCaseCB.IsChecked == true || LowerCaseCB.IsChecked == true || NumbersCB.IsChecked == true || SymbolsCB.IsChecked == true)
+            if (UpperCaseCB.IsChecked == true || LowerCaseCB.IsChecked == true || NumbersCB.IsChecked == true || SymbolsCB.IsChecked == true)
             {
-                TB1.Text = r1.Generate(val, UpperCaseCB.IsChecked, LowerCaseCB.IsChecked, NumbersCB.IsChecked, SymbolsCB.IsChecked);
+                PasswordTB.Text = r1.Generate(val, UpperCaseCB.IsChecked, LowerCaseCB.IsChecked, NumbersCB.IsChecked, SymbolsCB.IsChecked);
             }
             else
             {
                 MessageBox.Show("Choose characters for password.");
             }
-        }
-
-        private void B2_Click(object sender, RoutedEventArgs e)
-        {
-            Clipboard.SetText(TB1.Text);
-        }
-
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            val = Convert.ToInt32(S1.Value);
-            TB2.Text = val.ToString();
-        }
-    }
-
-    class RandomPaswwordGen
-    {
-        
-        public string Generate(int length, bool? cb1, bool? cb2, bool? cb3, bool? cb4)
-        {
-            Random rnd = new Random();
-            string symbols = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
-            string password = "";
-
-            for(int i = 0; i < length; i++)
-            {
-                int random = rnd.Next(4);
-                if (random == 0)
-                {
-                    if(cb1 == true)
-                    {
-                        char letter = Convert.ToChar(rnd.Next('A', 'Z' + 1));
-                        password += letter;
-                    }
-                    else
-                    {
-                        i--;
-                    }
-                }
-                else if(random == 1)
-                {
-                    if (cb2 == true)
-                    {
-                        char letter = Convert.ToChar(rnd.Next('a', 'z' + 1));
-                        password += letter;
-                    }
-                    else
-                    {
-                        i--;
-                    }
-                }
-                else if (random == 2)
-                {
-                    if (cb3 == true)
-                    {
-                        string number = rnd.Next(10).ToString();
-                        password += number;
-                    }
-                    else
-                    {
-                        i--;
-                    }
-                }
-                else
-                {
-                    if (cb4 == true)
-                    {
-                        char symbol = Convert.ToChar(symbols[rnd.Next(symbols.Length)]);
-                        password += symbol;
-                    }
-                    else
-                    {
-                        i--;
-                    }
-                }
-            }
-            return password;
         }
     }
 }
---
+++ Password Generator/RandomPaswwordGen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Password_Generator
+{
+    class RandomPaswwordGen
+    {
+        
+        public string Generate(int length, bool? cb1, bool? cb2, bool? cb3, bool? cb4)
+        {
+            Random rnd = new Random();
+            string symbols = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+            string password = "";
+
+            for(int i = 0; i < length; i++)
+            {
+                int random = rnd.Next(4);
+                if (random == 0)
+                {
+                    if(cb1 == true)
+                    {
+                        char letter = Convert.ToChar(rnd.Next('A', 'Z' + 1));
+                        password += letter;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+                else if(random == 1)
+                {
+                    if (cb2 == true)
+                    {
+                        char letter = Convert.ToChar(rnd.Next('a', 'z' + 1));
+                        password += letter;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+                else if (random == 2)
+                {
+                    if (cb3 == true)
+                    {
+                        string number = rnd.Next(10).ToString();
+                        password += number;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+                else
+                {
+                    if (cb4 == true)
+                    {
+                        char symbol = Convert.ToChar(symbols[rnd.Next(symbols.Length)]);
+                        password += symbol;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+            }
+            return password;
+        }
+    }
+}
